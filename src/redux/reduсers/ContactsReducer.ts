6b800f5ffa43fe29a2a4b59473a49cd1eda64978
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-26 20:53:28 +0300

    refactor(reducer, action): Рефактор типов.

--- src/redux/reduсers/ContactsReducer.ts
+++ src/redux/reduсers/ContactsReducer.ts
@@ -1,10 +1,7 @@
 import { DATA_CONTACT } from 'src/__data__'
-import { FIND_CONTACTS_NAME_ACTION } from '../Action'
+import { FIND_CONTACTS_NAME_ACTION, ProjectAction } from '../Action'
 
-export function contactsReducer(
-  state = DATA_CONTACT,
-  action: { type: any; payload: any }
-) {
+export function contactsReducer(state = DATA_CONTACT, action: ProjectAction) {
   switch (action.type) {
     case FIND_CONTACTS_NAME_ACTION:
       return state.filter(
