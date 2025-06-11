285684f528542771c67c6f3f6516fefa632e9c5c
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-06-11 21:31:16 +0300

    refactor: Поправить типы.

--- src/mobX/api.ts
+++ src/mobX/api.ts
@@ -1,13 +1,17 @@
+import { ContactDto } from 'src/types/dto/ContactDto'
+import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
+import { Response } from 'src/types/response'
+
 class Api {
-  async getContacts() {
+  async getContacts(): Promise<Response<ContactDto[]>> {
     const data = await this.fetch(
       'https://fs04.gcfiles.net/fileservice/file/download/a/177331/sc/385/h/0afc05779dcbbebd7055a1d87b8c7c6b.json'
     )
-
-    return data 
+    
+    return data
   }
 
-  async getGroupContacts() {
+  async getGroupContacts(): Promise<Response<GroupContactsDto[]>> {
     const data = await this.fetch(
       'https://fs04.gcfiles.net/fileservice/file/download/a/177331/sc/0/h/f1e98b0d70d16a909818b03b72415733.json'
     )
